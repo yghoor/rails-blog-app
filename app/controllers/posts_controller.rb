9fc38a61f85e43ec66721f2fd6c68de215405f44
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-20 22:46:57 +0200

    Implement pagination in posts index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
+  POSTS_PER_PAGE = 2
+
   def index
     @user = User.find(params[:user_id])
+    @page = params.fetch(:page, 0).to_i
+    @user_posts = @user.posts.all.order(created_at: :desc).offset(@page * POSTS_PER_PAGE).limit(POSTS_PER_PAGE)
   end
 
   def show
