b4ca1207cd040a37b55cbae4200ba874583f896d
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-25 00:06:08 +0200

    Build functionality for adding a like to a post

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,9 @@
 class LikesController < ApplicationController
+  def create
+    @post = Post.find(params[:post_id])
+    @like = current_user.likes.new(post_id: @post.id)
+
+    Like.create(author: current_user, post: @post)
+    redirect_to user_post_path(params[:user_id], params[:post_id])
+  end
 end
