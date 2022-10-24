f1c5bf8267d7b33bf48640f7336d33af2932600c
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-24 23:57:02 +0200

    Add functionality for creating new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,26 @@ class PostsController < ApplicationController
     @user_posts = @user.posts.all.order(created_at: :desc).offset(@page * POSTS_PER_PAGE).limit(POSTS_PER_PAGE)
   end
 
+  def create
+    post = current_user.posts.new(post_params)
+    post.author = current_user
+    if post.save
+      flash[:success] = 'Post saved successfully'
+      redirect_to user_posts_path(current_user)
+    else
+      flash.now[:error] = 'Error: Post could not be saved'
+      render :new, locals: { post: @post }
+    end
+  end
+
   def show
     @post = Post.find(params[:id])
     @comments = @post.comments
+  end
+
+  private
+
+  def post_params
+    params.require(user_posts_url).permit(:title, :text)
   end
 end
