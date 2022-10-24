37d474dc8d443f5441c01f614fc8c9c4271ba306
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-25 00:03:47 +0200

    Add functionality for creating a comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,4 +3,23 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
   end
 
+  def create
+    comment = current_user.comments.new(comment_params)
+    @post = Post.find(params[:post_id])
+    comment.post = @post
+
+    if comment.save
+      flash[:success] = 'Comment saved successfully'
+      redirect_to user_post_path(current_user, @post)
+    else
+      flash.now[:error] = 'Error: Comment could not be saved'
+      render :new, locals: { comment: }
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(user_post_comments_url).permit(:text)
+  end
 end
