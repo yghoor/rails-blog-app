69ff9a344b0b1618aec9c17d4a3770de332f24f5
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-26 11:32:53 +0200

    Add flash message to LikesController create method

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,13 @@
 class LikesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @like = current_user.likes.new(post_id: @post.id)
+    @like = Like.new(author: current_user, post: @post)
 
-    Like.create(author: current_user, post: @post)
+    if @like.save
+      flash[:success] = 'Like added successfully'
+    else
+      flash.now[:error] = 'Error: Like could not be saved'
+    end
     redirect_to user_post_path(params[:user_id], params[:post_id])
   end
 end
