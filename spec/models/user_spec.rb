ea245b6fcc2f42d6f859f70697f5b549c2c124c2
Yahya Ghoor <ghooryahya@gmail.com>
2022-10-26 11:23:34 +0200

    Write tests for User#recent_posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,4 +20,15 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
   end
+
+  context '#recent_posts' do
+    it 'returns the 3 most recent posts' do
+      user = User.create(name: 'John', photo_url: 'photo_url', bio: 'This is my bio')
+      Post.create(title: 'Title 1', text: 'Text 1', author: user)
+      post2 = Post.create(title: 'Title 2', text: 'Text 2', author: user)
+      post3 = Post.create(title: 'Title 3', text: 'Text 3', author: user)
+      post4 = Post.create(title: 'Title 4', text: 'Text 4', author: user)
+      expect(user.recent_posts).to eq([post4, post3, post2])
+    end
+  end
 end
