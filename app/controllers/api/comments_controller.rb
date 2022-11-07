cc1333d985cba5ee26e645acbf94d45da28d9dc8
Yahya Ghoor <ghooryahya@gmail.com>
2022-11-07 15:55:04 +0200

    Add functionality for creating a comment

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,8 +1,27 @@
 class Api::CommentsController < ApplicationController
   before_action :load_comments
+  before_action :ensure_params_exist, only: :create
 
   def index
     render json: @comments
+  end
+
+  def create
+    comment = Comment.new(author_id: comment_params[:user_id], post_id: comment_params[:post_id], text: comment_params[:text])
+
+    if comment.save
+      render json: {
+        messages: "Comment created successfully",
+        is_success: true,
+        data: {comment: comment}
+      }, status: :ok
+    else
+      render json: {
+        messages: "Comment creation failed",
+        is_success: false,
+        data: {}
+      }, status: :unprocessable_entity
+    end
   end
 
   private
@@ -10,5 +29,19 @@ class Api::CommentsController < ApplicationController
   def load_comments
     @post = Post.find(params[:post_id])
     @comments = @post.comments.order(created_at: :desc)
+  end
+
+  def comment_params
+    params.permit(:user_id, :post_id, :text)
+  end
+
+  def ensure_params_exist
+    return unless params.values_at(:user_id, :post_id, :text).include?(nil)
+
+    render json: {
+        messages: "Missing Params",
+        is_success: false,
+        data: {}
+      }, status: :bad_request
   end
 end
