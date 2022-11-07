f6cdf365b398c4490a27144e08c0332fea842c70
Yahya Ghoor <ghooryahya@gmail.com>
2022-11-07 15:53:42 +0200

    Build controller for fetching all posts a user

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,24 @@
+class Api::PostsController < ApplicationController
+  before_action :load_posts
+  before_action :ensure_params_exist
+  def index
+    render json: @all_posts
+  end
+
+  private
+
+  def load_posts
+    @user = User.find(params[:user_id])
+    @all_posts = @user.posts.all.order(created_at: :desc)
+  end
+
+  def ensure_params_exist
+    return unless params.values_at(:user_id).include?(nil)
+
+    render json: {
+        messages: "Missing Params",
+        is_success: false,
+        data: {}
+      }, status: :bad_request
+  end
+end
