0d720c13bed6a212f6c4041c554a8422bf85333e
Yahya Ghoor <ghooryahya@gmail.com>
2022-11-08 11:14:32 +0200

    Write api request specs for posts controller

---
+++ spec/requests/api/posts_spec.rb
@@ -0,0 +1,44 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/posts', type: :request do
+  path '/api/users/{user_id}/posts' do
+
+    get 'Retrieves posts for a user' do
+      produces 'application/json'
+      parameter name: 'user_id', in: :path, type: :string
+
+      response '200', 'Posts loaded successfully' do
+        schema type: :object,
+          properties: {
+            id: { type: :integer, },
+            author_id: { type: :integer },
+            title: { type: :string },
+            text: { type: :string },
+            comments_counter: { type: :integer },
+            likes_counter: { type: :integer }
+          }
+
+        let(:user_id) { 
+          @user = User.create(name: 'user', email: 'user@example.com', password: 'password',
+          password_confirmation: 'password').id
+        }
+        run_test!
+      end
+
+      response(400, 'Missing Params') do
+        schema type: :object,
+          properties: {
+            messages: { type: :string },
+            is_success: { type: :boolean },
+            data: { type: :object }
+          }
+
+          let(:user_id) { 
+            @user = User.create(name: 'user', email: 'user@example.com', password: 'password',
+            password_confirmation: 'password').id
+          }
+        run_test!
+      end
+    end
+  end
+end
