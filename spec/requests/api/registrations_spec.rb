780d6fc29bb10d8a3ce057a40bd8600988e141da
Yahya Ghoor <ghooryahya@gmail.com>
2022-11-08 17:12:03 +0200

    Write api request specs for RegistrationController

--- spec/requests/api/registrations_spec.rb
+++ spec/requests/api/registrations_spec.rb
@@ -2,56 +2,51 @@ require 'swagger_helper'
 
 RSpec.describe 'api/registrations', type: :request do
   # rubocop:disable Metrics/BlockLength
-  post 'Creates a user' do
-    produces 'application/json'
-    consumes 'application/json'
-    parameter name: :name, in: :body, type: :string
-    parameter name: :email, in: :body, type: :string
-    parameter name: :password, in: :body, type: :string
-    parameter name: :password_confirmation, in: :body, type: :string
+  path '/api/sign_up' do
+    parameter name: 'name', in: :query, type: :string, description: 'name'
+    parameter name: 'email', in: :query, type: :string, description: 'email'
+    parameter name: 'password', in: :query, type: :string, description: 'password'
+    parameter name: 'password_confirmation', in: :query, type: :string, description: 'password_confirmation'
 
-    response(200, 'Signed up successfully') do
-      schema type: :object,
-             properties: {
-               id: { type: :integer },
-               name: { type: :string },
-               email: { type: :string }
-             }
-
-      let(:name) { 'user' }
-      let(:email) { 'user@example.com' }
-      let(:password) { 'password' }
-      let(:password_confirmation) { 'password' }
-      run_test!
+    post 'Creates a user' do
+      produces 'application/json'
+      consumes 'application/json'
+      parameter name: :name, in: :body, type: :string
+      parameter name: :email, in: :body, type: :string
+      parameter name: :password, in: :body, type: :string
+      parameter name: :password_confirmation, in: :body, type: :string
+  
+      response(200, 'Signed up successfully') do
+        schema type: :object,
+               properties: {
+                 id: { type: :integer },
+                 name: { type: :string },
+                 email: { type: :string }
+               }
+  
+        let(:name) { 'apple' }
+        let(:email) { 'apple@example.com' }
+        let(:password) { 'password' }
+        let(:password_confirmation) { 'password' }
+        run_test!
+      end
     end
+  end
 
-    response(400, 'Missing Params') do
-      schema type: :object,
-             properties: {
-               messages: { type: :string },
-               is_success: { type: :boolean },
-               data: { type: :object }
-             }
-
-      let(:email) { 'user@example.com' }
-      let(:password) { 'password' }
-      let(:password_confirmation) { 'password' }
-      run_test!
-    end
-
-    response(422, 'Sign Up Failed') do
-      schema type: :object,
-             properties: {
-               messages: { type: :string },
-               is_success: { type: :boolean },
-               data: { type: :object }
-             }
-
-      let(:name) { 'user' }
-      let(:email) { 'user@example.com' }
-      let(:password) { 'password' }
-      let(:password_confirmation) { 'password' }
-      run_test!
+  path '/api/sign_up' do
+    post 'Creates a user' do
+      produces 'application/json'
+      consumes 'application/json'
+  
+      response(400, 'Missing Params') do
+        schema type: :object,
+               properties: {
+                 messages: { type: :string },
+                 is_success: { type: :boolean },
+                 data: { type: :object }
+               }
+        run_test!
+      end
     end
   end
   # rubocop:enable Metrics/BlockLength
